a693c13693a9385a4c7fd84fbc549dad19d0f6f9
Yan Schneider <yan.shnaider@itexus.com>
2021-11-15 14:59:31 +0100

    Initial Commit

---
+++ Kurlyk/KurlykApp.swift
@@ -0,0 +1,17 @@
+//
+//  KurlykApp.swift
+//  Kurlyk
+//
+//  Created by Yan Schneider on 15.11.21.
+//
+
+import SwiftUI
+
+@main
+struct KurlykApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
